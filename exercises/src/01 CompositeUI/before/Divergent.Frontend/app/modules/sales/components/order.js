306fc1af2b938b7dea727f506ced902a3ec3b6a5
Mauro Servienti <mauro@topics.it>
2016-12-28 16:11:48 +0100

    Removes templating engine to simplify the mechanics of the UI

---
+++ exercises/src/01 CompositeUI/before/Divergent.Frontend/app/modules/sales/components/order.js
@@ -0,0 +1,18 @@
+﻿/* global angular */
+(function () {
+
+    angular.module('app.services')
+        .directive('order', ['$log',
+            function ($log) {
+                $log.debug('order directive');
+
+                return {
+                    restrict: 'E',
+                    scope: {
+                        model: '=',
+                    },
+                    templateUrl: '/app/modules/sales/components/order.html'
+                };
+            }]);
+
+}())
