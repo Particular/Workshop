36615296a66fc6f45fa94cc24ef624f7e7492d30
Mauro Servienti <mauro@topics.it>
2017-01-18 18:18:47 +0100

    Sales data

---
+++ demos/CompositeUI-MVC/Divergent.Sales.Data/Migrations/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Divergent.Sales.Data.Models;
+
+namespace Divergent.Sales.Data.Migrations
+{
+    internal static class SeedData
+    {
+        internal static List<Order> Orders()
+        {
+            return new List<Order>()
+            {
+                new Order() { DateTimeUtc = new DateTime(2016, 01, 01), Items = new []
+                {
+                    new Item()
+                    {
+                        ProductId = 1
+                    },
+                    new Item()
+                    {
+                        ProductId = 2
+                    }
+                }},
+            };
+        }
+
+    }
+}
