e9226ebfaf49f5006b63b1cb48b32fe7afd15065
Mauro Servienti <mauro@topics.it>
2017-01-18 18:19:03 +0100

    Shipping data

---
+++ demos/CompositeUI-MVC/Divergent.Shipping.Data/Migrations/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using Divergent.Shipping.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Divergent.Shipping.Data.Migrations
+{
+    internal static class SeedData
+    {
+        internal static List<ShippingInfo> ShippingInfos()
+        {
+            return new List<ShippingInfo>()
+            {
+                new ShippingInfo() { OrderId = 1, Courier = "FedEx", Status = "Delivered"}
+            };
+        }
+    }
+}
