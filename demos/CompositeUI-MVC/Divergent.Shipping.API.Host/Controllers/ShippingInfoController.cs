60d8fa7e6fde942dbef1849596bb03ac18df3285
Mauro Servienti <mauro@topics.it>
2017-01-18 18:19:36 +0100

    Shipping API Host

---
+++ demos/CompositeUI-MVC/Divergent.Shipping.API.Host/Controllers/ShippingInfoController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using Divergent.Shipping.Data.Context;
+
+namespace Divergent.Shipping.API.Host.Controllers
+{
+    [RoutePrefix("api/shippinginfo")]
+    public class ShippingInfoController : ApiController
+    {
+        [HttpGet]
+        [Route("order/{id}")]
+        public dynamic Order(int id)
+        {
+            using (var db = new ShippingContext())
+            {
+                var info = db.ShippingInfos
+                    .Where(si => si.OrderId == id)
+                    .SingleOrDefault();
+
+                return info;
+            }
+        }
+    }
+}
