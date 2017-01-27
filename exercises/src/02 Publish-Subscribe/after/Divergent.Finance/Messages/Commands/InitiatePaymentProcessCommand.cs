e747a09cbdfd103f1885e1254f5d366c7664456e
Mauro Servienti <mauro@topics.it>
2017-01-27 15:29:04 +0100

    stores a snapshop of prices based on the submitted order

---
+++ exercises/src/02 Publish-Subscribe/after/Divergent.Finance/Messages/Commands/InitiatePaymentProcessCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divergent.Finance.Messages.Commands
+{
+    class InitiatePaymentProcessCommand
+    {
+        public double Amount { get; internal set; }
+        public Guid CustomerId { get; internal set; }
+        public Guid OrderId { get; internal set; }
+    }
+}
