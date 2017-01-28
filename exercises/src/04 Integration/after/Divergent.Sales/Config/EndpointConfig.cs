77294b8b82eb104dfdb2063f3a5f2a833fdb0c57
Mauro Servienti <mauro@topics.it>
2017-01-28 17:04:00 +0100

    Sales / after new design

--- exercises/src/04 Integration/after/Divergent.Sales/Config/EndpointConfig.cs
+++ exercises/src/04 Integration/after/Divergent.Sales/Config/EndpointConfig.cs
@@ -6,6 +6,7 @@ using NServiceBus.Features;
 using NServiceBus.Logging;
 using ILog = Common.Logging.ILog;
 using LogManager = Common.Logging.LogManager;
+using System.IO;
 
 namespace Divergent.Sales.Config
 {
@@ -40,6 +41,8 @@ namespace Divergent.Sales.Config
 
             var container = ContainerSetup.Create();
 
+            var licensePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "License.xml");
+            endpointConfiguration.LicensePath(licensePath);
             endpointConfiguration.UseSerialization<JsonSerializer>();
             endpointConfiguration.Recoverability().Delayed(c=>c.NumberOfRetries(0));
             endpointConfiguration.UseContainer<AutofacBuilder>(c => c.ExistingLifetimeScope(container));
