8667a32adc60b466634e85b9fe519c74a9a93572
Mauro Servienti <mauro@topics.it>
2017-01-28 15:53:37 +0100

    Customers / before new design

---
+++ exercises/src/03 Sagas/before/Divergent.Customers.Data/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divergent.Customers.Data.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
