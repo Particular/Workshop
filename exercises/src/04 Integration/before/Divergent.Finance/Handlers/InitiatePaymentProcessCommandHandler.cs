ca85c38a42c7f58a71c61c77ac009c472ab31183
Mauro Servienti <mauro@topics.it>
2017-01-28 17:31:54 +0100

    Finance / before new design

---
+++ exercises/src/04 Integration/before/Divergent.Finance/Handlers/InitiatePaymentProcessCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Divergent.Finance.Messages.Events;
+using Divergent.Finance.PaymentClient;
+using Divergent.Sales.Messages.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+using Divergent.Finance.Data.Context;
+using Divergent.Finance.Data.Models;
+using Divergent.Finance.Messages.Commands;
+
+namespace Divergent.Finance.Handlers
+{
+    class InitiatePaymentProcessCommandHandler : IHandleMessages<InitiatePaymentProcessCommand>
+    {
+        private static readonly ILog Log = LogManager.GetLogger<InitiatePaymentProcessCommand>();
+        private readonly ReliablePaymentClient _reliablePaymentClient;
+
+        public InitiatePaymentProcessCommandHandler(ReliablePaymentClient reliablePaymentClient)
+        {
+            _reliablePaymentClient = reliablePaymentClient;
+        }
+
+        public async Task Handle(InitiatePaymentProcessCommand message, IMessageHandlerContext context)
+        {
+            Log.Info("Handle InitiatePaymentProcessCommand");
+
+            await _reliablePaymentClient.ProcessPayment(message.CustomerId, message.Amount);
+
+            await context.Publish<PaymentSucceededEvent>(e =>
+            {
+                e.OrderId = message.OrderId;
+            });
+        }
+    }
+}
