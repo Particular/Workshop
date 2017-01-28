a674066be221acff3b5e4ae6f095585af43fdd20
Mauro Servienti <mauro@topics.it>
2017-01-28 17:04:28 +0100

    Shipping / after new design

--- exercises/src/04 Integration/after/Divergent.Shipping.Data/ITOps/ShippingInfoProvider.cs
+++ exercises/src/04 Integration/after/Divergent.Shipping.Data/ITOps/ShippingInfoProvider.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Divergent.ITOps.Interfaces;
+using System.Linq;
 
 namespace Divergent.Shipping.Data.ITOps
 {
     public class ShippingInfoProvider : IProvideShippingInfo
     {
-        public Task<PackageInfo> GetPackageInfo(List<Guid> productIds)
+        public Task<PackageInfo> GetPackageInfo(IEnumerable<int> productIds)
         {
+            var count = productIds.Count();
+
             return Task.FromResult(new PackageInfo()
             {
-                Weight = WeightCalculator.CalculateWeight(productIds.Count),
-                Volume = VolumeEstimator.Calculate(productIds.Count)
+                Weight = WeightCalculator.CalculateWeight(count),
+                Volume = VolumeEstimator.Calculate(count)
             });
         }
     }
