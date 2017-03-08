119442b1a2797fe34c62635abeb33ea56b95aeb5
Mauro Servienti <mauro@topics.it>
2017-03-08 19:31:47 +0100

    moved to localdb / after

--- exercises/src/02 Publish-Subscribe/after/Divergent.Finance.Data/Migrations/DatabaseInitializer.cs
+++ exercises/src/02 Publish-Subscribe/after/Divergent.Finance.Data/Migrations/DatabaseInitializer.cs
@@ -1,17 +1,11 @@
 ﻿using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
-using SQLite.CodeFirst;
 using Divergent.Finance.Data.Context;
 
 namespace Divergent.Finance.Data.Migrations
 {
-    public class DatabaseInitializer : SqliteCreateDatabaseIfNotExists<FinanceContext>
+    public class DatabaseInitializer : CreateDatabaseIfNotExists<FinanceContext>
     {
-        public DatabaseInitializer(DbModelBuilder modelBuilder) : base(modelBuilder)
-        {
-        }
-
         protected override void Seed(FinanceContext context)
         {
             context.Prices.AddOrUpdate(k => k.Id, SeedData.Prices().ToArray());
