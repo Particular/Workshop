913700eb458fea9d71f8130b2ccd5dc2e3432714
Mauro Servienti <mauro@topics.it>
2017-03-17 09:44:04 +0100

    localdb scripts and doco (#47)

---
+++ exercises/scripts/Setup.ps1
@@ -0,0 +1,11 @@
+#Requires -RunAsAdministrator
+
+$instanceName = "microservices-workshop"
+
+sqllocaldb create $instanceName
+sqllocaldb share $instanceName $instanceName
+sqllocaldb start $instanceName
+sqllocaldb info $instanceName
+
+$serverName = "(localdb)\" + $instanceName
+sqlcmd -S $serverName -i ".\Setup.sql"
