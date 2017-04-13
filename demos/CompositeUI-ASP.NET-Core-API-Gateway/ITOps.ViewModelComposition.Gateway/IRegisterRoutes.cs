3dfc457a648dce12380090b957395d7f7098b673
Mauro Servienti <mauro@topics.it>
2017-04-13 19:09:50 +0200

    Gateway hosting structure

---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/ITOps.ViewModelComposition.Gateway/IRegisterRoutes.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITOps.ViewModelComposition.Gateway
+{
+    public interface IRegisterRoutes
+    {
+        void RegisterRoutes(RouteBuilder routeBuilder, RouteHandler defaultGatewayRouteHandler);
+    }
+}
