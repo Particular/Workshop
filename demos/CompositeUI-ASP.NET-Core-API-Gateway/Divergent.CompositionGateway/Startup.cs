f3735b7df109e757e38842582fa2d88645195b13
Mauro Servienti <mauro@topics.it>
2017-04-13 19:09:50 +0200

    Gateway hosting structure

---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.CompositionGateway/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ITOps.ViewModelComposition.Gateway;
+
+namespace Divergent.CompositionGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddViewModelComposition();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.UseViewModelCompositionGateway();
+        }
+    }
+}
