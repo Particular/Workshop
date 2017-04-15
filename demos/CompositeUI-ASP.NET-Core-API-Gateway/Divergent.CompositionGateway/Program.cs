524da6a434823dd847a2e671508a27b02aaa1009
mauroservienti <mauro@topics.it>
2017-04-15 18:08:36 +0200

    adds back AppInsights

--- demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.CompositionGateway/Program.cs
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.CompositionGateway/Program.cs
@@ -16,6 +16,7 @@ namespace Divergent.CompositionGateway
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
