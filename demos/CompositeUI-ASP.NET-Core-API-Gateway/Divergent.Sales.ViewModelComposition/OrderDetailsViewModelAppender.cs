5d8957a83ac48c923aecab5f60e22f807a1ea81a
mauroservienti <mauro@topics.it>
2017-04-15 11:14:59 +0200

    add gateway routing support

---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.Sales.ViewModelComposition/OrderDetailsViewModelAppender.cs
@@ -0,0 +1,35 @@
+﻿using ITOps.ViewModelComposition;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Divergent.Sales.ViewModelComposition
+{
+    public class OrderDetailsViewModelAppender : IViewModelAppender
+    {
+        public bool Matches(RouteData routeData, string verb)
+        {
+            var controller = (string)routeData.Values["controller"];
+
+            return HttpMethods.IsGet(verb)
+                && controller.ToLowerInvariant() == "orders"
+                && routeData.Values.ContainsKey("id");
+        }
+
+        public Task Append(dynamic vm, RouteData routeData, IQueryCollection query)
+        {
+            var id = (string)routeData.Values["id"];
+
+            //var url = $"http://localhost:20195/api/orders/{id}";
+            //var client = new HttpClient();
+            //var response = await client.GetAsync(url).ConfigureAwait(false);
+
+            //dynamic order = await response.Content.AsExpandoAsync().ConfigureAwait(false);
+
+            vm.OrderNumber = id;
+            vm.OrderItemsCount = 120;
+
+            return Task.CompletedTask;
+        }
+    }
+}
