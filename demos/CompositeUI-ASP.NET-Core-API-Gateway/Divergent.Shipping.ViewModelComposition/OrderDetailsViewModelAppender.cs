eaac5fc9768c4f2c3560db5f467c58219c464f92
mauroservienti <mauro@topics.it>
2017-04-15 12:54:06 +0200

    support building lists via events

---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.Shipping.ViewModelComposition/OrderDetailsViewModelAppender.cs
@@ -0,0 +1,36 @@
+﻿using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Divergent.Shipping.ViewModelComposition
+{
+    public class OrderDetailsViewModelAppender : IViewModelAppender
+    {
+        public bool Matches(RouteData routeData, string verb)
+        {
+            var controller = (string)routeData.Values["controller"];
+
+            return HttpMethods.IsGet(verb)
+                && controller.ToLowerInvariant() == "orders"
+                && routeData.Values.ContainsKey("id");
+        }
+
+        public Task Append(dynamic vm, RouteData routeData, IQueryCollection query)
+        {
+            var id = (string)routeData.Values["id"];
+
+            //var url = $"http://localhost:20196/api/shippinginfo/order/{id}";
+            //var client = new HttpClient();
+            //var response = await client.GetAsync(url).ConfigureAwait(false);
+
+            //dynamic shipping = await response.Content.AsExpandoAsync().ConfigureAwait(false);
+
+            vm.ShippingStatus = "Shipped";
+            vm.ShippingCourier = "UPS";
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.Shipping.ViewModelComposition/OrdersLoadedSubscriber.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System;
+using Divergent.Sales.ViewModelComposition.Events;
+using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Threading.Tasks;
+
+namespace Divergent.Shipping.ViewModelComposition
+{
+    public class OrdersLoadedSubscriber : ISubscribeToCompositionEvents
+    {
+        public bool Matches(RouteData routeData, string httpVerb)
+        {
+            var controller = (string)routeData.Values["controller"];
+
+            return HttpMethods.IsGet(httpVerb)
+                && controller.ToLowerInvariant() == "orders"
+                && !routeData.Values.ContainsKey("id");
+        }
+
+        public void Subscribe(ISubscriptionStorage subscriptionStorage, RouteData rd, IQueryCollection queryString)
+        {
+            subscriptionStorage.Subscribe<OrdersLoaded>((pageViewModel, @event, routeData, query) =>
+            {
+                var ids = String.Join(",", @event.OrdersViewModel.Keys);
+
+                //var url = $"http://localhost:20196/api/shippinginfo/orders?ids={ids}";
+                //var client = new HttpClient();
+
+                //var response = await client.GetAsync(url).ConfigureAwait(false);
+
+                //dynamic[] shippingInfos = await response.Content.AsExpandoArrayAsync().ConfigureAwait(false);
+
+                //foreach (dynamic item in shippingInfos)
+                //{
+                //    @event.OrdersViewModel[item.OrderId].ShippingStatus = item.Status;
+                //    @event.OrdersViewModel[item.OrderId].ShippingCourier = item.Courier;
+                //}
+
+                foreach (var item in @event.OrdersViewModel)
+                {
+                    item.Value.ShippingStatus = $"Status of {item.Key}";
+                    item.Value.ShippingCourier = $"Courier of {item.Key}";
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
