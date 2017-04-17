f9c80b8e056aed6d1a8d6539441e5a84d142bdcd
mauroservienti <mauro@topics.it>
2017-04-17 10:31:01 +0200

    basic SPA Frontend structure

---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/Divergent.SPA/app/app.ts
@@ -0,0 +1,12 @@
+﻿class App
+{
+    host: HTMLElement;
+
+    public constructor(elementHost: HTMLElement) {
+        this.host = elementHost;
+    }
+
+    run() {
+        this.host.innerText = "running from typescript!";
+    }
+}
