70673a25eb2bfd907d6db8297e75e8f8d8482127
Mauro Servienti <mauro@topics.it>
2017-04-18 13:31:43 +0200

    add WebAPI Hosts to support Sales and Shipping data requests

---
+++ demos/CompositeUI-ASP.NET-Core-API-Gateway/ITOps.ViewModelComposition.Json/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace ITOps.ViewModelComposition.Json
+{
+    public static class StringExtensions
+    {
+        public static string ToPascalCase(this string s)
+        {
+            if(string.IsNullOrEmpty(s) || !char.IsLower(s[ 0 ]))
+                return s;
+
+            string str = char.ToUpperInvariant(s[ 0 ]).ToString();
+
+            if(s.Length > 1)
+                str = str + s.Substring(1);
+
+            return str;
+        }
+    }
+}
