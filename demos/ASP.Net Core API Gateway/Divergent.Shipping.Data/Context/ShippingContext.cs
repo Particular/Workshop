92b241d96ea1cddb8cdb19f22119b7767d2384b2
Mauro Servienti <mauro@topics.it>
2017-04-19 12:10:18 +0200

    remove and sort usings

--- demos/ASP.Net Core API Gateway/Divergent.Shipping.Data/Context/ShippingContext.cs
+++ demos/ASP.Net Core API Gateway/Divergent.Shipping.Data/Context/ShippingContext.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Divergent.Shipping.Data.Migrations;
+﻿using Divergent.Shipping.Data.Migrations;
 using Divergent.Shipping.Data.Models;
+using System.Data.Entity;
 
 namespace Divergent.Shipping.Data.Context
 {
