baa0883d43f3d1deaedf6be16ab5000f5b5e9a35
Mauro Servienti <mauro@topics.it>
2017-04-19 12:10:18 +0200

    remove and sort usings

--- demos/ASP.Net Core API Gateway/Divergent.Sales.API.Host/Program.cs
+++ demos/ASP.Net Core API Gateway/Divergent.Sales.API.Host/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Topshelf;
+﻿using Topshelf;
 
 namespace Divergent.Sales.API.Host
 {
