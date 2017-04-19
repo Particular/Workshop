def4968bd467ba17261c73954f453fc73363e8de
Mauro Servienti <mauro@topics.it>
2017-04-19 16:58:07 +0200

    small refactorings, mostly to expression bodied members

--- demos/ASP.Net Core API Gateway/ITOps.ViewModelComposition.Json/HttpContentExtensions.cs
+++ demos/ASP.Net Core API Gateway/ITOps.ViewModelComposition.Json/HttpContentExtensions.cs
@@ -7,18 +7,10 @@ namespace ITOps.ViewModelComposition.Json
 {
     public static class HttpContentExtensions
     {
-        public static async Task<ExpandoObject> AsExpandoAsync(this HttpContent content)
-        {
-            var obj = JsonConvert.DeserializeObject<ExpandoObject>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
-
-            return obj;
-        }
-
-        public static async Task<ExpandoObject[]> AsExpandoArrayAsync(this HttpContent content)
-        {
-            var obj = JsonConvert.DeserializeObject<ExpandoObject[]>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
-
-            return obj;
-        }
+        public static async Task<ExpandoObject> AsExpandoAsync(this HttpContent content) 
+            => JsonConvert.DeserializeObject<ExpandoObject>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
+        
+        public static async Task<ExpandoObject[]> AsExpandoArrayAsync(this HttpContent content) 
+            => JsonConvert.DeserializeObject<ExpandoObject[]>(await content.ReadAsStringAsync(), CamelCaseToPascalSettings.GetSerializerSettings());
     }
 }
