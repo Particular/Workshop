b94930d6b73508cc9d031ea2871f6cc64473763e
Mauro Servienti <mauro@topics.it>
2017-04-19 16:37:27 +0200

    refactors Subscription classes into their own files

---
+++ demos/ASP.Net Core API Gateway/ITOps.ViewModelComposition/Subscription.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITOps.ViewModelComposition
+{
+    abstract class Subscription
+    {
+        public abstract Task Invoke(dynamic viewModel, object @event, RouteData routeData, IQueryCollection query);
+    }
+}
