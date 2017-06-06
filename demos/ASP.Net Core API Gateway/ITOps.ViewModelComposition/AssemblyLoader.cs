77a53bd67b18de4144a6210c14d1e69fa47e854f
mauroservienti <mauro@topics.it>
2017-06-06 11:34:42 +0200

    Fix #64 assembly loading bug

---
+++ demos/ASP.Net Core API Gateway/ITOps.ViewModelComposition/AssemblyLoader.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyModel;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ITOps.ViewModelComposition
+{
+    public static class AssemblyLoader
+    {
+        public static Assembly Load(string assemblyFullPath)
+        {
+            var fileNameWithOutExtension = Path.GetFileNameWithoutExtension(assemblyFullPath);
+
+            var inCompileLibraries= DependencyContext.Default.CompileLibraries.Any(l => l.Name.Equals(fileNameWithOutExtension, StringComparison.OrdinalIgnoreCase));
+            var inRuntimeLibraries = DependencyContext.Default.RuntimeLibraries.Any(l => l.Name.Equals(fileNameWithOutExtension, StringComparison.OrdinalIgnoreCase));
+
+            var assembly = (inCompileLibraries || inRuntimeLibraries)
+                ? Assembly.Load(new AssemblyName(fileNameWithOutExtension))
+                : AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyFullPath);
+
+            return assembly;
+        }
+    }
+}
