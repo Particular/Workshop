442f7d6d2b08b80e1b7d7096256e5c6da402bab8
Mauro Servienti <mauro.servienti@gmail.com>
2017-07-27 19:10:17 +0200

    retieve data, log returned results and iterate over orders

--- demos/asp-net-core/Divergent.SPA.Frontend/app/main.ts
+++ demos/asp-net-core/Divergent.SPA.Frontend/app/main.ts
@@ -3,9 +3,18 @@ import Config from "./Config";
 import { httpGetRequest } from "./http";
 
 $(async () => {
-    window.console.log("Init...", Config.gatewayBaseUrl);
-
     const data = await httpGetRequest<any>(Config.gatewayBaseUrl + "/orders");
+    const orders: any[] = data.orders;
 
-    window.console.log("data: ", data);
+    $("#app").empty();
+    const appContainer: HTMLElement = $("#app");
+
+    window.console.log("appContainer", appContainer);
+    window.console.log("Data", data);
+    window.console.log("orders", orders);
+    
+    for (var i = 0; i < orders.length; i++) {
+        const o = orders[i];
+        window.console.log("order", o);
+    }
 });
