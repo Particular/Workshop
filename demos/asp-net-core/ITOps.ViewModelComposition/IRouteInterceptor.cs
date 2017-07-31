c656716d94411c7e56e37e3714b5d24b5273fecc
Mauro Servienti <mauro.servienti@gmail.com>
2017-07-31 09:33:26 +0200

    Extract interfaces into their own files

--- demos/asp-net-core/ITOps.ViewModelComposition/IRouteInterceptor.cs
+++ demos/asp-net-core/ITOps.ViewModelComposition/IRouteInterceptor.cs
@@ -1,21 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Routing;
 
 namespace ITOps.ViewModelComposition
 {
     public interface IRouteInterceptor
     {
         bool Matches(RouteData routeData, string httpVerb);
-    }
-
-    public interface ISubscribeToCompositionEvents : IRouteInterceptor
-    {
-        void Subscribe(ISubscriptionStorage subscriptionStorage, RouteData routeData, IQueryCollection query);
-    }
-
-    public interface IViewModelAppender : IRouteInterceptor
-    {
-        Task Append(dynamic vm, RouteData routeData, IQueryCollection query);
-    }
+    }    
 }
