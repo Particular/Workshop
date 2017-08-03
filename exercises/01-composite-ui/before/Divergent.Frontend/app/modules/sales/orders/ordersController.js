b6b47e7dbd2f9cdba49a96d4b650c3365d1fa0b0
Mauro Servienti <mauro.servienti@gmail.com>
2017-08-03 19:04:27 +0200

    Changing loading spinner to be a directive, using cg-busy

--- exercises/01-composite-ui/before/Divergent.Frontend/app/modules/sales/orders/ordersController.js
+++ exercises/01-composite-ui/before/Divergent.Frontend/app/modules/sales/orders/ordersController.js
@@ -6,19 +6,17 @@
 
                 var ctrl = this;
 
-                ctrl.loadingData = true;
+                ctrl.isLoading = null;
                 ctrl.orders = null;
 
-                backendCompositionService
+                ctrl.isLoading = backendCompositionService
                     .get('orders-list', { pageIndex: 0, pageSize: 10 })
                     .then(function (viewModel) {
                         ctrl.orders = viewModel.orders;
                     })
                     .catch(function (error) {
-                        $log.error('Ooops, something went wroing: ', error);
-                    })
-                    .finally(function () {
-                        ctrl.loadingData = false;
+                        $log.error('Something went wrong: ', error);
+                        ctrl.loadError = 'Something went wrong. Look at the console log in your browser';
                     });
 
             }]);
