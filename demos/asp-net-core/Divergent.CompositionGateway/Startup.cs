39ff2d20fc3f5933699c4974b2c63f0a6b77aefd
Mauro Servienti <mauro.servienti@gmail.com>
2017-10-13 07:03:50 +0200

    Add configuration support

--- demos/asp-net-core/Divergent.CompositionGateway/Startup.cs
+++ demos/asp-net-core/Divergent.CompositionGateway/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using ITOps.ViewModelComposition;
 using ITOps.ViewModelComposition.Gateway;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -8,8 +10,20 @@ namespace Divergent.CompositionGateway
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IConfiguration>(Configuration);
             services.AddRouting();
             services.AddViewModelComposition();
         }
