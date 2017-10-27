7a082bc4eb5dafa22d80dd3a8e88661c5a34733c
Adam Ralph <adam@adamralph.com>
2017-10-27 16:42:23 +0200

    refactor ApplicationBuilderExtensions

--- demos/asp-net-core/ITOps.ViewModelComposition.Gateway/ApplicationBuilderExtensions.cs
+++ demos/asp-net-core/ITOps.ViewModelComposition.Gateway/ApplicationBuilderExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using System;
@@ -8,19 +8,19 @@ namespace ITOps.ViewModelComposition.Gateway
 {
     public static class ApplicationBuilderExtensions
     {
-        public static void RunCompositionGateway(this IApplicationBuilder app, Action<IRouteBuilder> routes = null)
+        public static void RunCompositionGateway(this IApplicationBuilder app, Action<IRouteBuilder> configureRouteBuilder = null)
         {
             var routeBuilder = new RouteBuilder(app);
-            routes?.Invoke(routeBuilder);
+            configureRouteBuilder?.Invoke(routeBuilder);
 
             app.UseRouter(routeBuilder.Build());
         }
 
         public static void RunCompositionGatewayWithDefaultRoutes(this IApplicationBuilder app) =>
-            app.RunCompositionGateway(routes =>
+            app.RunCompositionGateway(routeBuilder =>
             {
-                routes.MapComposableGet(template: "{controller}/{id:int?}");
-                routes.MapRoute("*", context =>
+                routeBuilder.MapComposableGet(template: "{controller}/{id:int?}");
+                routeBuilder.MapRoute("*", context =>
                 {
                     context.Response.StatusCode = StatusCodes.Status404NotFound;
                     return Task.CompletedTask;
