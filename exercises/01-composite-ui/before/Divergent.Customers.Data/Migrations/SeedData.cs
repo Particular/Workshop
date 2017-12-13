9fc92783692c7d982c4057f1feb1621e8c9709e5
Mauro Servienti <mauro.servienti@gmail.com>
2017-12-13 14:16:37 +0100

    Before: Align Customers data

---
+++ exercises/01-composite-ui/before/Divergent.Customers.Data/Migrations/SeedData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Divergent.Customers.Data.Models;
+
+namespace Divergent.Customers.Data.Migrations
+{
+    internal static class SeedData
+    {
+        internal static List<Customer> Customers()
+        {
+            var particularId = 1;
+            var particularOrderId = 1;
+
+            var nserviceBusId = 2;
+            var nserivceBusOrderId = 2;
+
+            var acmeId = 3;
+
+            return new List<Customer>
+            {
+                    new Customer
+                    {
+                        Id = particularId,
+                        Name = "Particular Software ",
+                        Orders = new List<Order>
+                        {
+                            new Order { CustomerId = particularId, OrderId = particularOrderId }
+                        }
+                    },
+                    new Customer
+                    {
+                        Id = nserviceBusId,
+                        Name = "NServiceBus Ltd.",
+                        Orders = new List<Order>
+                        {
+                            new Order { CustomerId = nserviceBusId, OrderId = nserivceBusOrderId }
+                        }
+                    },
+                    new Customer { Id = acmeId, Name = "Acme Inc.", Orders = new List<Order>() }
+            };
+        }
+    }
+}
