136c0331e30b38fe9175a8b252c0b6fb3f644492
Mauro Servienti <mauro.servienti@gmail.com>
2017-12-13 12:14:59 +0100

    After: Align Customers data

---
+++ exercises/01-composite-ui/after/Divergent.Customers.Data/Context/CustomersContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using Divergent.Customers.Data.Models;
+using Divergent.Customers.Data.Migrations;
+
+namespace Divergent.Customers.Data.Context
+{
+    public class CustomersContext : DbContext
+    {
+        public CustomersContext() : base("Divergent.Customers")
+        {
+        }
+
+        public IDbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(e => e.Orders)
+                .WithRequired()
+                .HasForeignKey(k => k.CustomerId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- exercises/01-composite-ui/after/Divergent.Customers.Data/Models/Customer.cs
+++ exercises/01-composite-ui/after/Divergent.Customers.Data/Models/Customer.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Divergent.Customers.Data.Models
 {
     public class Customer
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string PostalCode { get; set; }
         public string Country { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
     }
 }
