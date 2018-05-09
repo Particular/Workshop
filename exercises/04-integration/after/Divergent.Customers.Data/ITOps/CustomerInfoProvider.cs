1ca0f0b1d9a5f5c943923915abe61d83c342436b
danielmarbach <daniel.marbach@openplace.net>
2018-05-09 10:48:18 +0200

    make CustomerInfoProvider async

--- exercises/04-integration/after/Divergent.Customers.Data/ITOps/CustomerInfoProvider.cs
+++ exercises/04-integration/after/Divergent.Customers.Data/ITOps/CustomerInfoProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using Divergent.ITOps.Interfaces;
 
@@ -6,20 +7,20 @@ namespace Divergent.Customers.Data.ITOps
 {
     public class CustomerInfoProvider : IProvideCustomerInfo
     {
-        public Task<CustomerInfo> GetCustomerInfo(int customerId)
+        public async Task<CustomerInfo> GetCustomerInfo(int customerId)
         {
             using (var db = new Context.CustomersContext())
             {
-                var customer = db.Customers.Where(c => c.Id == customerId).Single();
+                var customer = await db.Customers.Where(c => c.Id == customerId).SingleAsync();
 
-                return Task.FromResult(new CustomerInfo
+                return new CustomerInfo
                 {
                     Name = customer.Name,
                     Street = customer.Street,
                     City = customer.City,
                     PostalCode = customer.PostalCode,
                     Country = customer.Country,
-                });
+                };
             }
         }
     }
