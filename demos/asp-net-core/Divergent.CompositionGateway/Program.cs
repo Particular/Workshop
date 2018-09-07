96bfa1260095e25f3596308f4ad472c40c9adf4e
Adam Ralph <adam@adamralph.com>
2018-09-07 18:03:08 +0200

    remove App Insights from demo
    Co-authored-by: Mauro Servienti <mauro.servienti@gmail.com>

--- demos/asp-net-core/Divergent.CompositionGateway/Program.cs
+++ demos/asp-net-core/Divergent.CompositionGateway/Program.cs
@@ -12,7 +12,6 @@ namespace Divergent.CompositionGateway
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
--- demos/asp-net-core/Divergent.Website/Program.cs
+++ demos/asp-net-core/Divergent.Website/Program.cs
@@ -12,7 +12,6 @@ namespace Divergent.Website
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
