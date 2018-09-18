b990365b95534081b78250b6af71519096e7264b
Adam Ralph <adam@adamralph.com>
2018-09-18 14:34:30 +1000

    add Finance service

---
+++ demos/asp-net-core/Finance.ViewModelComposition/OrderDetailsViewModelAppender.cs
@@ -0,0 +1,23 @@
+﻿using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Threading.Tasks;
+
+namespace Finance.ViewModelComposition
+{
+    public class OrderDetailsViewModelAppender : IViewModelAppender
+    {
+        public Task Append(dynamic viewModel, RouteData routeData, IQueryCollection query)
+        {
+            // go to back the end get the prices for each order...
+            viewModel.Price = 100;
+            return Task.CompletedTask;
+        }
+
+        public bool Matches(RouteData routeData, string httpMethod)=>
+            HttpMethods.IsGet(httpMethod)
+                && string.Equals((string)routeData.Values["controller"], "orders", StringComparison.OrdinalIgnoreCase)
+                && routeData.Values.ContainsKey("id");
+    }
+}
---
+++ demos/asp-net-core/Finance.ViewModelComposition/OrdersLoadedSubscriber.cs
@@ -0,0 +1,33 @@
+﻿using Divergent.Sales.ViewModelComposition.Events;
+using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Finance.ViewModelComposition
+{
+    class OrdersLoadedSubscriber : ISubscribeToCompositionEvents
+    {
+        public bool Matches(RouteData routeData, string httpMethod) =>
+HttpMethods.IsGet(httpMethod)
+                && string.Equals((string)routeData.Values["controller"], "orders", StringComparison.OrdinalIgnoreCase)
+                && !routeData.Values.ContainsKey("id");
+
+        public void Subscribe(IPublishCompositionEvents publisher)
+        {
+            publisher.Subscribe<OrdersLoaded>(async (pageViewModel, ordersLoaded, routeData, query) =>
+            {
+                var orderNumbers = string.Join(",", ordersLoaded.OrderViewModelDictionary.Keys);
+
+// use the order numbers to find the prices
+
+                foreach (dynamic order in ordersLoaded.OrderViewModelDictionary)
+                {
+                    order.Value.Price = 100;
+                }
+            });
+        }
+    }
+}
