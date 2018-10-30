ac8149176267c6797cea27e09cadc60ad04f6041
Adam Ralph <adam@adamralph.com>
2018-10-30 12:42:31 +0100

    switch to SimpleExec

--- targets/Program.cs
+++ targets/Program.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Diagnostics;
 using System.IO;
 using static Bullseye.Targets;
+using static SimpleExec.Command;
 
 internal class Program
 {
@@ -12,36 +11,13 @@ internal class Program
         Target(
             "demos",
             Directory.EnumerateFiles("demos", "*.sln", SearchOption.AllDirectories),
-            solution => Cmd("dotnet", $"build {solution} --configuration Debug"));
+            solution => Run("dotnet", $"build {solution} --configuration Debug"));
 
         Target(
             "exercises",
             Directory.EnumerateFiles("exercises", "*.sln", SearchOption.AllDirectories),
-            solution => Cmd("dotnet", $"build {solution} --configuration Debug"));
+            solution => Run("dotnet", $"build {solution} --configuration Debug"));
 
         RunTargets(args);
-    }
-
-    private static void Cmd(string fileName, string args)
-    {
-        using (var process = new Process())
-        {
-            process.StartInfo = new ProcessStartInfo
-            {
-                FileName = $"\"{fileName}\"",
-                Arguments = args,
-                UseShellExecute = false,
-            };
-
-            Console.WriteLine($"Running '{process.StartInfo.FileName} {process.StartInfo.Arguments}'...");
-            process.Start();
-
-            process.WaitForExit();
-
-            if (process.ExitCode != 0)
-            {
-                throw new InvalidOperationException($"The command exited with code {process.ExitCode}.");
-            }
-        }
     }
 }
