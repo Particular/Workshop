edaf7add628031fdf8d41d5e5ddbb6316b8fa07e
Mauro Servienti <mauro.servienti@gmail.com>
2023-01-09 15:52:25 +0100

    Convert Shipping API to ASP.Net Core web API

---
+++ demos/asp-net-core/Divergent.Shipping.API/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Divergent.Shipping.API
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
