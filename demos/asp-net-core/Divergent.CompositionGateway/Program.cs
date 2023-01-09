7ea95ad12fb0ae75c5026db58ce617831d2e7fee
Mauro Servienti <mauro.servienti@gmail.com>
2023-01-09 15:50:05 +0100

    Convert CompositionGateway to use ServiceComposer

--- demos/asp-net-core/Divergent.CompositionGateway/Program.cs
+++ demos/asp-net-core/Divergent.CompositionGateway/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.IO;
 
 namespace Divergent.CompositionGateway
@@ -7,14 +9,11 @@ namespace Divergent.CompositionGateway
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
