d2039fd02c6f7f494ee27178769829a47cb1fa74
Dennis <dvdstelt@gmail.com>
2023-01-10 14:08:30 +0100

    Empty 03 before

--- exercises/03-sagas/before/Divergent.Finance.Messages/Events/PaymentSucceededEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Divergent.Finance.Messages.Events
-{
-    public class PaymentSucceededEvent
-    {
-        public int OrderId { get; set; }
-    }
-}
--- exercises/03-sagas/before/Divergent.Sales.Data/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Divergent.Sales.Data.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
