1dfea448a33d78e9ff0497a9a324b1f9475e63b0
Dennis <dvdstelt@gmail.com>
2023-01-10 14:30:44 +0100

    02 after emptied

--- exercises/02-publish-subscribe/after/Divergent.Customers.Data/Models/Order.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Divergent.Customers.Data.Models
-{
-    public class Order
-    {
-        public int Id { get; set; }
-        public int CustomerId { get; set; }
-        public int OrderId { get; set; }
-    }
-}
--- exercises/02-publish-subscribe/after/Divergent.Finance/Messages/Commands/InitiatePaymentProcessCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Divergent.Finance.Messages.Commands
-{
-    public class InitiatePaymentProcessCommand
-    {
-        public double Amount { get; set; }
-        public int CustomerId { get; set; }
-        public int OrderId { get; set; }
-    }
-}
