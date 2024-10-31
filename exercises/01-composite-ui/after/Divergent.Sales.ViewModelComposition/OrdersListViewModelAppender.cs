eeffdc8c0e20ffd3f3a3b35c2a7ef20650c0f554
David Boike <david.boike@gmail.com>
2024-10-31 11:41:22 -0500

    Exercise 1 after

--- exercises/01-composite-ui/after/Divergent.Sales.ViewModelComposition/OrdersListViewModelAppender.cs
+++ exercises/01-composite-ui/after/Divergent.Sales.ViewModelComposition/OrdersListViewModelAppender.cs
@@ -1,24 +1,19 @@
-﻿using Divergent.Sales.ViewModelComposition.Events;
-using ITOps.Json;
-using ITOps.ViewModelComposition;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Dynamic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Divergent.Sales.ViewModelComposition.Events;
+using ITOps.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceComposer.AspNetCore;
 
 namespace Divergent.Sales.ViewModelComposition;
 
-public class OrdersListViewModelAppender : IViewModelAppender
+public class OrdersListViewModelAppender : ICompositionRequestsHandler
 {
-    public bool Matches(RouteData routeData, string httpMethod) =>
-        HttpMethods.IsGet(httpMethod)
-        && string.Equals((string)routeData.Values["controller"], "orders", StringComparison.OrdinalIgnoreCase)
-        && !routeData.Values.ContainsKey("id");
-
-    public async Task Append(dynamic viewModel, RouteData routeData, IQueryCollection query)
+    [HttpGet("/orders")]
+    public async Task Handle(HttpRequest request)
     {
         // Hardcoded for simplicity. In a production app, a config object could be injected.
         var url = $"http://localhost:20185/api/orders";
@@ -28,8 +23,10 @@ public class OrdersListViewModelAppender : IViewModelAppender
 
         var orderViewModelDictionary = MapToViewModelDictionary(orders);
 
-        await viewModel.RaiseEventAsync(new OrdersLoaded { OrderViewModelDictionary = orderViewModelDictionary });
+        var compositionContext = request.GetCompositionContext();
+        await compositionContext.RaiseEvent(new OrdersLoaded { OrderViewModelDictionary = orderViewModelDictionary });
 
+        var viewModel = request.GetComposedResponseModel();
         viewModel.Orders = orderViewModelDictionary.Values;
     }
 
